c31f6fc476de896336b7f2075b1826ff711bf0be
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-11-07 14:48:11 +0000

    em teste

---
+++ script.js
@@ -0,0 +1,9 @@
+function Acender(){
+    document.getElementById("Árvore de natal").src ="on.png"
+    console.log("On")
+}
+
+function Apagar(){
+    document.getElementById("Árvore de natal").src ="off.png"
+    console.log("off")
+}
