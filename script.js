7a77e64f62c8cd48c32e389ec3bb206d9ee42f0e
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-11-10 14:09:58 +0000

    concluido

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function Desenfeitar(){
     document.getElementById("Árvore de natal").src ="off.png"
     console.log("off")
 }
+
+function Presentear(){
+    document.getElementById("Árvore de natal").src ="presentear.png"
+    console.log("presentear")
+}
